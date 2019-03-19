3859e2f56ad3e5dd6cd822f3b646bef3ae464135
Lakhiwe L. Liwani <lakhiwe@explore-datascience.net>
2019-03-19 15:47:25 +0200

    fixed the sorting function

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -53,47 +53,22 @@ print(items)
 
 
 
-def quickSort(items):
-   quickSortHelper(items,0,len(items)-1)
+def quicksort(items):
+    if len(items) == 1 or len(items) == 0:
+        return items
+    else:
+        pivot = items[0]
+        i = 0
+        for j in range(len(items)-1):
+            if items[j+1] < pivot:
+                items[j+1],items[i+1] = items[i+1], items[j+1]
+                i += 1
+        items[0],items[i] = items[i],items[0]
+        first_part = quicksort(items[:i])
+        second_part = quicksort(items[i+1:])
+        first_part.append(items[i])
+        return first_part + second_part
 
-def quickSortHelper(items,first,last):
-   if first<last:
-
-       splitpoint = partition(items,first,last)
-
-       quickSortHelper(items,first,splitpoint-1)
-       quickSortHelper(items,splitpoint+1,last)
-
-
-def partition(items,first,last):
-   pivotvalue = items[first]
-
-   leftmark = first+1
-   rightmark = last
-
-   done = False
-   while not done:
-
-       while leftmark <= rightmark and items[leftmark] <= pivotvalue:
-           leftmark = leftmark + 1
-
-       while items[rightmark] >= pivotvalue and rightmark >= leftmark:
-           rightmark = rightmark -1
-
-       if rightmark < leftmark:
-           done = True
-       else:
-           temp = items[leftmark]
-           items[leftmark] = items[rightmark]
-           items[rightmark] = temp
-
-   temp = items[first]
-   items[first] = items[rightmark]
-   items[rightmark] = temp
-
-
-   return rightmark
-
-items = [54,26,93,17,77,31,44,55,20, 100, 107]
-quickSort(items)
-print(items)
+alist = [54,26,93,17,77,31,44,55,20]
+quickSort(alist)
+print(alist)
